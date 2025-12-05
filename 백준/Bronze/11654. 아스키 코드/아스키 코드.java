0f18507a75296f2a80e4a1a6427ce3b5e4bbae1c
ibtSdan <ibtsdan@gmail.com>
2025-12-05 16:22:38 +0900

    [Bronze V] Title: 아스키 코드, Time: 172 ms, Memory: 17592 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String text = scanner.next();
+        System.out.println((int) text.charAt(0));
+    }
+}
