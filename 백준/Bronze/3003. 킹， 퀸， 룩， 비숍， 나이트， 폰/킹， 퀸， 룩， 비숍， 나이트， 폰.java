78d6a64f1601cce71801096e0a753f3afe607deb
ibtSdan <ibtsdan@gmail.com>
2025-12-05 20:14:57 +0900

    [Bronze V] Title: 킹, 퀸, 룩, 비숍, 나이트, 폰, Time: 184 ms, Memory: 18328 KB -BaekjoonHub

--- 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.java
+++ 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.java
@@ -3,29 +3,10 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int king = scanner.nextInt();
-        if (king != 1) {
-            System.out.print((1-king) + " ");
-        } else System.out.print(0+ " ");
-        int q = scanner.nextInt();
-        if (q != 1) {
-            System.out.print((1-q) + " ");
-        } else System.out.print(0+ " ");
-        int l = scanner.nextInt();
-        if (l != 2) {
-            System.out.print((2 - l) + " ");
-        } else System.out.print(0+ " ");
-        int v = scanner.nextInt();
-        if (v != 2) {
-            System.out.print((2 - v) + " ");
-        } else System.out.print(0+ " ");
-        int n = scanner.nextInt();
-        if (n != 2) {
-            System.out.print((2-n)+" ");
-        } else System.out.print(0+" ");
-        int p = scanner.nextInt();
-        if (p != 8) {
-            System.out.println((8 - p)+" ");
-        } else System.out.println(0);
+        int []data = {1,1,2,2,2,8};
+
+        for (int i = 0; i<6; i++) {
+            System.out.print(data[i] - scanner.nextInt() + " ");
+        }
     }
 }
