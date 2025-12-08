6d81d7a770404e18d66f2ea7532ae2cc7a4856f6
ibtSdan <ibtsdan@gmail.com>
2025-12-08 14:24:50 +0900

    [Bronze IV] Title: 심부름 가는 길, Time: 124 ms, Memory: 15924 KB -BaekjoonHub

---
+++ 백준/Bronze/5554. 심부름 가는 길/심부름 가는 길.java
@@ -0,0 +1,15 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int total = 0;
+        for (int i = 0; i < 4; i ++) {
+            total += Integer.parseInt(br.readLine());
+        }
+        bw.write(total/60+"\n"+total%60+"\n");
+        bw.flush();
+    }
+}
