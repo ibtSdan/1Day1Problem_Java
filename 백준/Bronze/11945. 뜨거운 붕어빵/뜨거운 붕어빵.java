07aab0d56f25c26850fbf992730127dd7551cb32
ibtSdan <ibtsdan@gmail.com>
2025-12-08 23:54:54 +0900

    [Bronze IV] Title: 뜨거운 붕어빵, Time: 100 ms, Memory: 14208 KB -BaekjoonHub

---
+++ 백준/Bronze/11945. 뜨거운 붕어빵/뜨거운 붕어빵.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        for (int i = 0; i < n; i++) {
+            String temp = new StringBuilder(br.readLine()).reverse().toString();
+            bw.write(temp+"\n");
+        }
+        bw.flush();
+    }
+}
