2a845de0c5c83fbeb7d3abd465a7c54488993ff9
ibtSdan <ibtsdan@gmail.com>
2025-12-12 11:49:08 +0900

    [Bronze II] Title: 하얀 칸, Time: 116 ms, Memory: 15924 KB -BaekjoonHub

---
+++ 백준/Bronze/1100. 하얀 칸/하얀 칸.java
@@ -0,0 +1,21 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        int cnt = 0;
+        for (int i = 0; i < 8; i++) {
+            String line = br.readLine();
+            for (int j = 0; j < 8; j++) {
+                char ch = line.charAt(j);
+                if ((i+j)%2 == 0 && ch == 'F') {
+                    cnt++;
+                }
+            }
+        }
+        bw.write(cnt+"\n");
+        bw.flush();
+    }
+}
