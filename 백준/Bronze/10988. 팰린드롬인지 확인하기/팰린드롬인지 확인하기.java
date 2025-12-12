22c939e73a964325674542e6de5721a813977209
ibtSdan <ibtsdan@gmail.com>
2025-12-12 11:58:18 +0900

    [Bronze III] Title: 팰린드롬인지 확인하기, Time: 100 ms, Memory: 14272 KB -BaekjoonHub

---
+++ 백준/Bronze/10988. 팰린드롬인지 확인하기/팰린드롬인지 확인하기.java
@@ -0,0 +1,18 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        String line = br.readLine();
+        String rev = new StringBuilder(line).reverse().toString();
+        
+        if (line.equals(rev)) {
+            bw.write(1+"\n");
+        } else {
+            bw.write(0+"\n");
+        }
+        bw.flush();
+    }
+}
