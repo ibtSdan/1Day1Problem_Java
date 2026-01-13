6d249cea566f4271455c9cfc234de2ff1d0ebf95
ibtSdan <ibtsdan@gmail.com>
2026-01-13 17:17:27 +0900

    [Bronze I] Title: 평균, Time: 124 ms, Memory: 16008 KB -BaekjoonHub

--- 백준/Bronze/1546. 평균/평균.java
+++ 백준/Bronze/1546. 평균/평균.java
@@ -1,20 +1,26 @@
 import java.io.*;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
+
         int n = Integer.parseInt(br.readLine());
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int[] data = new int[n];
+
+        int sum = 0;
+        int max = 0;
         for (int i = 0; i < n; i++) {
-            data[i] = Integer.parseInt(st.nextToken());
+            int temp = Integer.parseInt(st.nextToken());
+            sum += temp;
+            if (temp > max) {
+                max = temp;
+            }
         }
-        int max = Arrays.stream(data).max().getAsInt();
-        double ans = Arrays.stream(data).sum()/(float)max*100/n;
+
+        double ans = (double) sum/max*100/n;
         bw.write(ans+"\n");
         bw.flush();
     }
