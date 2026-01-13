b64123e81a7988419d0c298554e6cc7326be3356
ibtSdan <ibtsdan@gmail.com>
2026-01-13 17:36:13 +0900

    [Bronze III] Title: 최소, 최대, Time: 540 ms, Memory: 118200 KB -BaekjoonHub

--- 백준/Bronze/10818. 최소， 최대/최소， 최대.java
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.java
@@ -5,16 +5,15 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
+        
         int n = Integer.parseInt(br.readLine());
+        List<Integer> data = new ArrayList<>();
         StringTokenizer st = new StringTokenizer(br.readLine());
-        List<Integer> list = new ArrayList<>();
         for (int i = 0; i < n; i++) {
-            list.add(Integer.parseInt(st.nextToken()));
+            data.add(Integer.parseInt(st.nextToken()));
         }
-
-        int min = Collections.min(list);
-        int max = Collections.max(list);
+        int min = Collections.min(data);
+        int max = Collections.max(data);
 
         bw.write(min+" "+max+"\n");
         bw.flush();
