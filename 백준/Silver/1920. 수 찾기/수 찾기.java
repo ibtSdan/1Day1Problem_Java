8df5563d1d3bd1f5a776c2809928ec11cf6536a9
ibtSdan <ibtsdan@gmail.com>
2026-01-19 16:12:25 +0900

    [Silver IV] Title: 수 찾기, Time: 616 ms, Memory: 42144 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.java
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -2,40 +2,48 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+    static int[] data;
+    static int n;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int n = Integer.parseInt(br.readLine());
-        int[] arr = new int[n];
+        n = Integer.parseInt(br.readLine());
+        data = new int [n];
         StringTokenizer st = new StringTokenizer(br.readLine());
         for (int i = 0; i < n; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
+            data[i] = Integer.parseInt(st.nextToken());
         }
-        Arrays.sort(arr);
+        Arrays.sort(data);
 
         int m = Integer.parseInt(br.readLine());
-        StringTokenizer st1 = new StringTokenizer(br.readLine());
+        st = new StringTokenizer(br.readLine());
         for (int i = 0; i < m; i++) {
-            int target = Integer.parseInt(st1.nextToken());
-            bw.write(search(arr, target)+"\n");
+            int k = Integer.parseInt(st.nextToken());
+            boolean ans = Find(k);
+            if (ans) {
+                bw.write("1\n");
+            } else {
+                bw.write("0\n");
+            }
         }
         bw.flush();
     }
 
-    static int search(int[] arr, int target) {
-        int left = 0;
-        int right = arr.length - 1;
-        while (left <= right) {
-            int mid = (left + right) / 2;
-            if (arr[mid] == target) {
-                return 1;
-            } else if (arr[mid] > target) {
-                right = mid - 1;
+    static boolean Find(int k) {
+        int s = 0;
+        int e = n-1;
+        while (s <= e) {
+            int mid = (s+e)/2;
+            if (data[mid] == k) {
+                return true;
+            } else if (data[mid] > k) {
+                e = mid - 1;
             } else {
-                left = mid + 1;
+                s = mid + 1;
             }
         }
-        return 0;
+        return false;
     }
 }
