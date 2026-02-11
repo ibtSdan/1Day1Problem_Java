f19c58ed5284fce243f124c66e91f8bc2d337eef
ibtSdan <ibtsdan@gmail.com>
2026-02-11 17:11:58 +0900

    [Silver III] Title: 바이러스, Time: 100 ms, Memory: 14396 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.java
+++ 백준/Silver/2606. 바이러스/바이러스.java
@@ -2,42 +2,45 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+    static BufferedWriter bw;
     static boolean[] visited;
-    static ArrayList<Integer>[] graph;
-    static int count = 0;
+    static ArrayList<Integer>[] A;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        
+        bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
         int n = Integer.parseInt(br.readLine());
         int m = Integer.parseInt(br.readLine());
+        A = new ArrayList[n+1];
+        visited = new boolean[n+1];
+        for (int i = 1; i <= n; i++) {
+            A[i] = new ArrayList<>();
+        }
+        for (int i = 0; i < m; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int u = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+            A[u].add(v);
+            A[v].add(u);
+        }
 
-         visited = new boolean[n+1];
-         graph = new ArrayList[n+1];
-         for (int i = 1; i <= n; i++) {
-             graph[i] = new ArrayList<>();
-         }
-         for (int i = 0; i < m; i++) {
-             StringTokenizer st = new StringTokenizer(br.readLine());
-             int a = Integer.parseInt(st.nextToken());
-             int b = Integer.parseInt(st.nextToken());
-             graph[a].add(b);
-             graph[b].add(a);
-         }
-
-         dfs(1);
-         bw.write(count-1+"\n");
-         bw.flush();
-    }
-
-    static void dfs(int x) {
-        visited[x] = true;
-        count++;
-        for (int next : graph[x]) {
-            if (!visited[next]) {
-                dfs(next);
+        Deque<Integer> dq = new ArrayDeque<>();
+        dq.addLast(1);
+        visited[1] = true;
+        int cnt = 0;
+        while (!dq.isEmpty()) {
+            int now = dq.pollFirst();
+            for (int i : A[now]) {
+                if (!visited[i]) {
+                    dq.addLast(i);
+                    visited[i] = true;
+                    cnt++;
+                }
             }
         }
+
+        bw.write(cnt+"\n");
+        bw.flush();
     }
 }
