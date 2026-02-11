9562bd9a3b332c2fac6fd1a156cd43a4e4360d19
ibtSdan <ibtsdan@gmail.com>
2026-02-11 13:27:04 +0900

    [Silver IV] Title: 수 찾기, Time: 544 ms, Memory: 41420 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.java
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -2,27 +2,26 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int[] data;
+    static BufferedWriter bw;
     static int n;
+    static int[] A;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
+        bw = new BufferedWriter(new OutputStreamWriter(System.out));
         n = Integer.parseInt(br.readLine());
-        data = new int [n];
+        A = new int[n];
         StringTokenizer st = new StringTokenizer(br.readLine());
         for (int i = 0; i < n; i++) {
-            data[i] = Integer.parseInt(st.nextToken());
+            A[i] = Integer.parseInt(st.nextToken());
         }
-        Arrays.sort(data);
-
+        Arrays.sort(A);
         int m = Integer.parseInt(br.readLine());
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < m; i++) {
             int k = Integer.parseInt(st.nextToken());
-            boolean ans = Find(k);
-            if (ans) {
+            boolean check = search(k);
+            if (check) {
                 bw.write("1\n");
             } else {
                 bw.write("0\n");
@@ -31,14 +30,14 @@ public class Main {
         bw.flush();
     }
 
-    static boolean Find(int k) {
+    static boolean search(int k) {
         int s = 0;
         int e = n-1;
         while (s <= e) {
             int mid = (s+e)/2;
-            if (data[mid] == k) {
+            if (A[mid] == k) {
                 return true;
-            } else if (data[mid] > k) {
+            } else if (A[mid] > k) {
                 e = mid - 1;
             } else {
                 s = mid + 1;
