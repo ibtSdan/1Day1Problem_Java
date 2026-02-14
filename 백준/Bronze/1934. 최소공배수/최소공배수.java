c18d624b0084d890b94308e6af6948e80c1e9d17
ibtSdan <ibtsdan@gmail.com>
2026-02-14 19:23:59 +0900

    [Bronze I] Title: 최소공배수, Time: 136 ms, Memory: 16124 KB -BaekjoonHub

---
+++ 백준/Bronze/1934. 최소공배수/최소공배수.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static BufferedWriter bw;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int T = Integer.parseInt(br.readLine());
+        for (int t = 0; t < T; t++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            bw.write(a*b/gcd(a,b)+"\n");
+        }
+        bw.flush();
+    }
+
+    static int gcd(int a, int b) {
+        if (b == 0) {
+            return a;
+        }
+        return gcd(b, a%b);
+    }
+}
