dfed6a0de2411feb0fa39d7c49ee777c31a043f8
ibtSdan <ibtsdan@gmail.com>
2026-02-22 21:22:13 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 0.02 ms, Memory: 86.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int[] solution(int[] num_list) {
+        int even = 0, odd = 0;
+        for (int i : num_list) {
+            if (i%2==0) {
+                even++;
+            } else {
+                odd++;
+            }
+        }
+        return new int[] {even, odd};
+    }
+}
