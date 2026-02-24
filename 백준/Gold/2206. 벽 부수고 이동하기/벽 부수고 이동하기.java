d21a0769c263ebce0a8353f9b9fc26740079160d
ibtSdan <ibtsdan@gmail.com>
2026-02-24 10:49:50 +0900

    [Gold III] Title: 벽 부수고 이동하기, Time: 740 ms, Memory: 139064 KB -BaekjoonHub

--- 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.java
+++ 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.java
@@ -10,32 +10,25 @@ public class Main {
         StringTokenizer st = new StringTokenizer(br.readLine());
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
-
-        int[][] A = new int[n][m];
+        int[][] A = new int[n+1][m+1];
         for (int i = 0; i < n; i++) {
-            char[] c = br.readLine().toCharArray();
+            String line = br.readLine();
             for (int j = 0; j < m; j++) {
-                A[i][j] = c[j] - '0';
+                A[i][j] = line.charAt(j) - '0';
             }
         }
-        if (n == 1 && m == 1) {
-            System.out.println(1);
-            return;
-        }
-
-        int[] dx = new int[] {1,-1,0,0};
-        int[] dy = new int[] {0,0,1,-1};
-
+        int[] dx = {1,-1,0,0};
+        int[] dy = {0,0,1,-1};
         Deque<int[]> dq = new ArrayDeque<>();
-        boolean[][][] visited = new boolean[n][m][2];
         dq.addLast(new int[] {0,0,0});
+        boolean[][][] visited = new boolean[n][m][2];
         visited[0][0][0] = true;
         int[][][] dist = new int[n][m][2];
         dist[0][0][0] = 1;
 
         while (!dq.isEmpty()) {
             int[] now = dq.pollFirst();
-            int i = now[0], j = now[1], used = now[2]; //0 안부숨, 1 부숨
+            int i = now[0], j = now[1], used = now[2];
             for (int k = 0; k < 4; k++) {
                 int x = i + dx[k];
                 int y = j + dy[k];
@@ -43,25 +36,27 @@ public class Main {
                     if (A[x][y] == 1 && used == 1) {
                         continue;
                     } else if (A[x][y] == 1 && used == 0 && !visited[x][y][1]) {
+                        visited[x][y][1] = true;
                         dist[x][y][1] = dist[i][j][0] + 1;
                         dq.addLast(new int[] {x,y,1});
-                        visited[x][y][1] = true;
                     } else if (A[x][y] == 0 && !visited[x][y][used]) {
+                        visited[x][y][used] = true;
                         dist[x][y][used] = dist[i][j][used] + 1;
                         dq.addLast(new int[] {x,y,used});
-                        visited[x][y][used] = true;
                     }
                 }
             }
         }
+
         int a = dist[n-1][m-1][0];
         int b = dist[n-1][m-1][1];
         if (a == 0 && b == 0) {
-            System.out.println(-1);
-        } else if (a == 0 || b == 0){
-            System.out.println(Math.max(a,b));
+            bw.write("-1\n");
+        } else if (a == 0 || b == 0) {
+            bw.write(Math.max(a,b)+"\n");
         } else {
-            System.out.println(Math.min(a,b));
+            bw.write(Math.min(a,b)+"\n");
         }
+        bw.flush();
     }
 }
