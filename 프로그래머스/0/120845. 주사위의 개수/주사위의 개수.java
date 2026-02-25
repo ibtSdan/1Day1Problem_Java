f0912e2257af92ce9be08301b2eabf9ada011e6b
ibtSdan <ibtsdan@gmail.com>
2026-02-25 16:10:09 +0900

    [level 0] Title: 주사위의 개수, Time: 0.02 ms, Memory: 84.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/120845. 주사위의 개수/주사위의 개수.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int solution(int[] box, int n) {
+        int garo = box[0]/n;
+        int sero = box[1]/n;
+        int height = box[2]/n;
+        return garo * sero * height;
+    }
+}
