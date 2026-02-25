0af87a343feafd147afc2c87b80f240bdc1fc652
ibtSdan <ibtsdan@gmail.com>
2026-02-25 23:43:50 +0900

    [level 0] Title: 꼬리 문자열, Time: 7.65 ms, Memory: 87.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/181841. 꼬리 문자열/꼬리 문자열.java
@@ -0,0 +1,11 @@
+class Solution {
+    public String solution(String[] str_list, String ex) {
+        String ans = "";
+        for (String s : str_list) {
+            if (!s.contains(ex)) {
+                ans += s;
+            }
+        }
+        return ans;
+    }
+}
