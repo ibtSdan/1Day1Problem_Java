f6ae6ff70020801fdc4239a81660bc6b2611b1aa
ibtSdan <ibtsdan@gmail.com>
2026-02-25 23:26:35 +0900

    [level 0] Title: 숫자 찾기, Time: 0.03 ms, Memory: 81.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120904. 숫자 찾기/숫자 찾기.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(int num, int k) {
+        int ans = -1;
+        String s = String.valueOf(num);
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) - '0' == k) {
+                ans = i+1;
+                break;
+            }
+        }
+        return ans;
+    }
+}
