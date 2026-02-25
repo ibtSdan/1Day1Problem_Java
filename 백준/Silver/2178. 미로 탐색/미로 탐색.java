21957496929197b07126edce549d62e3c4f33922
ibtSdan <ibtsdan@gmail.com>
2026-02-25 13:54:43 +0900

    [Silver I] Title: 미로 탐색, Time: 128 ms, Memory: 16192 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.java
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -3,49 +3,41 @@ import java.util.*;
 
 public class Main {
     static BufferedWriter bw;
-    static boolean[][] visited;
-    static int[][] A;
-    static int[] dx = {1,-1,0,0};
-    static int[] dy = {0,0,1,-1};
-    static int n, m;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        n = Integer.parseInt(st.nextToken());
-        m = Integer.parseInt(st.nextToken());
-
-        visited = new boolean[n][m];
-        A = new int[n][m];
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int[][] A = new int[n][m];
+        boolean[][] visited = new boolean[n][m];
+        int[][] dist = new int[n][m];
         for (int i = 0; i < n; i++) {
             String line = br.readLine();
             for (int j = 0; j < m; j++) {
-                A[i][j] = Integer.parseInt(line.substring(j,j+1));
+                A[i][j] = line.charAt(j) - '0';
             }
         }
-        BFS(0,0);
-        bw.write(A[n-1][m-1]+"\n");
-        bw.flush();
-    }
-
-    static void BFS(int i, int j) {
+        dist[0][0] = 1;
         Deque<int[]> dq = new ArrayDeque<>();
-        dq.addLast(new int[]{i, j});
-        visited[i][j] = true;
+        dq.addLast(new int[] {0,0});
+        visited[0][0] = true;
+        int[] dx = {1,-1,0,0};
+        int[] dy = {0,0,1,-1};
         while (!dq.isEmpty()) {
             int[] now = dq.pollFirst();
+            int i = now[0], j = now[1];
             for (int k = 0; k < 4; k++) {
-                int x = dx[k] + now[0];
-                int y = dy[k] + now[1];
-                if (x >= 0 && x < n && y >= 0 && y < m ) {
-                    if (A[x][y] != 0 && !visited[x][y]) {
-                        A[x][y] = A[now[0]][now[1]] + 1;
-                        visited[x][y] = true;
-                        dq.addLast(new int[] {x, y});
-                    }
+                int x = i + dx[k], y = j + dy[k];
+                if (x >= 0 && x < n && y >= 0 && y < m && A[x][y] == 1 && !visited[x][y]) {
+                    visited[x][y] = true;
+                    dq.addLast(new int[] {x,y});
+                    dist[x][y] = dist[i][j] + 1;
                 }
             }
         }
+        bw.write(dist[n-1][m-1]+"\n");
+        bw.flush();
     }
 }
