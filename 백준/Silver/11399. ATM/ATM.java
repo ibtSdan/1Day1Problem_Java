00b85d94c48ae1e21559bb9f6eefee083ea1a47f
ibtSdan <ibtsdan@gmail.com>
2026-02-25 13:59:12 +0900

    [Silver IV] Title: ATM, Time: 128 ms, Memory: 15904 KB -BaekjoonHub

--- 백준/Silver/11399. ATM/ATM.java
+++ 백준/Silver/11399. ATM/ATM.java
@@ -2,24 +2,26 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+    static BufferedWriter bw;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
+        bw = new BufferedWriter(new OutputStreamWriter(System.out));
         int n = Integer.parseInt(br.readLine());
-        int[] data = new int[n];
+        int[] A = new int[n];
         StringTokenizer st = new StringTokenizer(br.readLine());
         for (int i = 0; i < n; i++) {
-            data[i] = Integer.parseInt(st.nextToken());
+            A[i] = Integer.parseInt(st.nextToken());
         }
-        Arrays.sort(data);
+        Arrays.sort(A);
         int[] S = new int[n];
-        S[0] = data[0];
-        int ans = S[0];
-
+        S[0] = A[0];
         for (int i = 1; i < n; i++) {
-            S[i] = S[i-1] + data[i];
-            ans += S[i];
+            S[i] = S[i-1] + A[i];
+        }
+        int ans = 0;
+        for (int i : S) {
+            ans += i;
         }
         bw.write(ans+"\n");
         bw.flush();
