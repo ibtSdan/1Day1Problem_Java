d4fb96e14e5215f6f5406303d1dd6c3728074e33
ibtSdan <ibtsdan@gmail.com>
2026-02-26 11:20:47 +0900

    [Gold IV] Title: 회전 초밥, Time: 1012 ms, Memory: 326516 KB -BaekjoonHub

--- 백준/Gold/15961. 회전 초밥/회전 초밥.java
+++ 백준/Gold/15961. 회전 초밥/회전 초밥.java
@@ -12,14 +12,14 @@ public class Main {
         int d = Integer.parseInt(st.nextToken());
         int k = Integer.parseInt(st.nextToken());
         int c = Integer.parseInt(st.nextToken());
-
-        int[] A = new int[N+(k-1)];
+        int[] A = new int[N+k-1];
         for (int i = 0; i < N; i++) {
             A[i] = Integer.parseInt(br.readLine());
         }
         for (int i = N; i < N+k-1; i++) {
             A[i] = A[i-N];
         }
+
         Map<Integer, Integer> map = new HashMap<>();
         for (int i = 0; i < k; i++) {
             map.put(A[i], map.getOrDefault(A[i], 0)+1);
@@ -28,26 +28,24 @@ public class Main {
         if (!map.containsKey(c)) {
             cnt++;
         }
-        int max = cnt;
+        int ans = cnt;
 
         int s = 0, e = k;
-        while (e <= N+k-2) {
-            map.put(A[e], map.getOrDefault(A[e],0)+1);
-            map.put(A[s], map.get(A[s]) - 1);
+        while (e < N+k-1) {
+            map.put(A[e], map.getOrDefault(A[e], 0) + 1);
+            map.put(A[s], map.get(A[s])-1);
             if (map.get(A[s]) == 0) {
                 map.remove(A[s]);
             }
-
             cnt = map.size();
             if (!map.containsKey(c)) {
                 cnt++;
             }
-            max = Math.max(max, cnt);
+            ans = Math.max(ans, cnt);
             s++;
             e++;
         }
-
-        bw.write(max+"\n");
+        bw.write(ans+"\n");
         bw.flush();
     }
 }
