78ae207c7c85baca6d22ff056750df51d1f13ac6
ibtSdan <ibtsdan@gmail.com>
2026-02-27 23:24:34 +0900

    [Gold V] Title: 최소비용 구하기, Time: 492 ms, Memory: 51440 KB -BaekjoonHub

--- 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.java
+++ 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.java
@@ -3,58 +3,53 @@ import java.util.*;
 
 public class Main {
     static BufferedWriter bw;
-
     static class Edge {
         int to, weight;
-        Edge (int to, int weight) {
-            this.to = to;
-            this.weight = weight;
+        Edge(int t, int w) {
+            to = t;
+            weight = w;
         }
     }
-
-    static final int INF = Integer.MAX_VALUE;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        int N = Integer.parseInt(br.readLine());
-        int M = Integer.parseInt(br.readLine());
-
-        ArrayList<Edge>[] graph = new ArrayList[N+1];
-        for (int i = 1; i <= N; i++) {
-            graph[i] = new ArrayList<>();
+        int n = Integer.parseInt(br.readLine());
+        int m = Integer.parseInt(br.readLine());
+        ArrayList<Edge>[] A = new ArrayList[n+1];
+        for (int i = 1; i <= n; i++) {
+            A[i] = new ArrayList<>();
         }
-        for (int i = 0; i < M; i++) {
+        int[] d = new int[n+1];
+        Arrays.fill(d, Integer.MAX_VALUE);
+        for (int i = 0; i < m; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
             int u = Integer.parseInt(st.nextToken());
             int v = Integer.parseInt(st.nextToken());
             int w = Integer.parseInt(st.nextToken());
-            graph[u].add(new Edge(v, w));
+            A[u].add(new Edge(v, w));
         }
         StringTokenizer st = new StringTokenizer(br.readLine());
         int start = Integer.parseInt(st.nextToken());
         int end = Integer.parseInt(st.nextToken());
-
-        int[] dist = new int[N+1];
-        Arrays.fill(dist, INF);
-        dist[start] = 0;
-
-        PriorityQueue<int[]> pq = new PriorityQueue<>(Comparator.comparingInt(a -> a[0]));
+        d[start] = 0;
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b) -> { return a[0] - b[0];});
         pq.add(new int[] {0, start});
         while (!pq.isEmpty()) {
             int[] now = pq.poll();
             int now_d = now[0], now_v = now[1];
-            if (dist[now_v] < now_d) continue;
-            for (Edge e : graph[now_v]) {
-                int next_v = e.to, w = e.weight;
-                if (dist[next_v] > dist[now_v] + w) {
-                    dist[next_v] = dist[now_v] + w;
-                    pq.add(new int[] {dist[next_v], next_v});
+            if (now_d > d[now_v]) {
+                continue;
+            }
+            for (Edge e : A[now_v]) {
+                int to = e.to, weight = e.weight;
+                if (d[to] > now_d+weight) {
+                    d[to] = now_d + weight;
+                    pq.add(new int[] {d[to], to});
                 }
             }
         }
-
-        bw.write(dist[end]+"\n");
+        bw.write(d[end]+"\n");
         bw.flush();
     }
 }
