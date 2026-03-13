1a08dd3d3e7c710ce0eadf1857ed9ee80aa18237
ibtSdan <ibtsdan@gmail.com>
2026-03-13 16:19:56 +0900

    [level 1] Title: 덧칠하기, Time: 2.41 ms, Memory: 95.4 MB -BaekjoonHub

--- 프로그래머스/1/161989. 덧칠하기/덧칠하기.java
+++ 프로그래머스/1/161989. 덧칠하기/덧칠하기.java
@@ -1,23 +1,12 @@
-import java.util.*;
-
 class Solution {
     public int solution(int n, int m, int[] section) {
-        ArrayList<Integer> A = new ArrayList<>();
-        for (int i : section) {
-            A.add(i);
-        }
-        int cnt = 0;
-        while (!A.isEmpty()) {
-            int num = A.get(0);
-            for (int i = num; i < num+m; i++) {
-                if (A.size() == 0) {
-                    break;
-                }
-                if (i == A.get(0)) {
-                    A.remove(0);
-                }
+        int cnt = 1;
+        int roller = section[0];
+        for (int i = 0; i < section.length; i++) {
+            if (roller + m - 1 < section[i]) {
+                cnt++;
+                roller = section[i];
             }
-            cnt++;
         }
         return cnt;
     }
