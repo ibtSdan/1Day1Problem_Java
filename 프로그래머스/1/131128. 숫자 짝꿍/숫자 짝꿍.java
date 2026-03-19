3211318c3a1fa3bb293316e2f0e624b46179871a
ibtSdan <ibtsdan@gmail.com>
2026-03-19 18:37:48 +0900

    [level 1] Title: 숫자 짝꿍, Time: 79.47 ms, Memory: 159 MB -BaekjoonHub

---
+++ 프로그래머스/1/131128. 숫자 짝꿍/숫자 짝꿍.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String X, String Y) {
+        int[] x = new int[10];
+        int[] y = new int[10];
+        for (char c : X.toCharArray()) {
+            x[c-'0']++;
+        }
+        for (char c : Y.toCharArray()) {
+            y[c-'0']++;
+        }
+        String ans = "";
+        for (int i = 9; i >= 0; i--) {
+            int min = Math.min(x[i], y[i]);
+            if (min != 0) {
+                ans += String.valueOf(i).repeat(min);
+            }
+        }
+        if (ans.equals("")) {
+            ans = "-1";
+        } else if (ans.compareTo("1") < 0) {
+            ans = "0";
+        }
+        return ans; 
+    }
+}
