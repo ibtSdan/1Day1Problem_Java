9d59626be7d581378d45714edf07b5a2f165a262
ibtSdan <ibtsdan@gmail.com>
2026-03-24 16:03:16 +0900

    [Silver I] Title: 단지번호붙이기, Time: 124 ms, Memory: 16088 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
@@ -3,66 +3,58 @@ import java.util.*;
 
 public class Main {
     static BufferedWriter bw;
-    static boolean[][] visited;
     static int[][] A;
+    static boolean[][] visited;
     static int[] dx = {1,-1,0,0};
     static int[] dy = {0,0,1,-1};
-    static int cnt, n;
-    static ArrayList<Integer> ans;
-
+    static ArrayList<Integer> ans = new ArrayList<>();
+    static int n;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
         n = Integer.parseInt(br.readLine());
-        visited = new boolean[n][n];
         A = new int[n][n];
         for (int i = 0; i < n; i++) {
-            String st = br.readLine();
+            String line = br.readLine();
             for (int j = 0; j < n; j++) {
-                A[i][j] = Integer.parseInt(st.substring(j,j+1));
+                A[i][j] = Integer.parseInt(line.substring(j, j+1));
             }
         }
-
-        ans = new ArrayList<>();
-
+        int cnt = 0;
+        visited = new boolean[n][n];
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
                 if (A[i][j] == 1 && !visited[i][j]) {
                     cnt++;
-                    BFS(i,j);
+                    BFS(i, j);
                 }
             }
         }
-
-        bw.write(cnt+"\n");
         Collections.sort(ans);
+        bw.write(cnt+"\n");
         for (int i : ans) {
             bw.write(i+"\n");
         }
         bw.flush();
     }
 
-    static void BFS(int i, int j) {
-        int house = 1;
+    static void BFS(int u, int v) {
         Deque<int[]> dq = new ArrayDeque<>();
-        dq.addLast(new int[] {i, j});
-        visited[i][j] = true;
+        dq.add(new int[] {u, v});
+        int cnt = 1;
+        visited[u][v] = true;
         while (!dq.isEmpty()) {
-            int[] now = dq.pollFirst();
+            int[] now = dq.poll();
             for (int k = 0; k < 4; k++) {
-                int x = now[0] + dx[k];
-                int y = now[1] + dy[k];
-                if (x >= 0 && x < n && y >= 0 && y < n) {
-                    if (A[x][y] == 1 && !visited[x][y]) {
-                        dq.addLast(new int[] {x, y});
-                        visited[x][y] = true;
-                        house++;
-                    }
+                int x = dx[k] + now[0], y = dy[k] + now[1];
+                if (x >= 0 && x < n && y >= 0 && y < n && A[x][y] == 1 && !visited[x][y]) {
+                    visited[x][y] = true;
+                    cnt++;
+                    dq.addLast(new int[] {x, y});
                 }
             }
         }
-        ans.add(house);
+        ans.add(cnt);
     }
 }
