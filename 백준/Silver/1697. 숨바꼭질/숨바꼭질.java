98e7e4d4609a62887fac583cf89331fc20f18fd3
ibtSdan <ibtsdan@gmail.com>
2026-03-24 16:46:18 +0900

    [Silver I] Title: 숨바꼭질, Time: 168 ms, Memory: 21700 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
@@ -8,26 +8,23 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st = new StringTokenizer(br.readLine());
-
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
-
-        boolean[] visited = new boolean[100001];
         Deque<int[]> dq = new ArrayDeque<>();
         dq.add(new int[] {n, 0});
+        boolean[] visited = new boolean[100001];
         visited[n] = true;
-
         while (!dq.isEmpty()) {
-            int[] now = dq.pollFirst();
+            int[] now = dq.poll();
             if (now[0] == m) {
                 bw.write(now[1]+"\n");
                 break;
             }
-            int[] range = {now[0]+1, now[0]-1, now[0]*2};
-            for (int i : range) {
+            int[] arr = {now[0]+1, now[0]-1, now[0]*2};
+            for (int i : arr) {
                 if (i >= 0 && i <= 100000 && !visited[i]) {
-                    visited[i] = true;
                     dq.add(new int[] {i, now[1]+1});
+                    visited[i] = true;
                 }
             }
         }
