99f9aa30f8263bce8ce5ba7591eeb0eb4e0d8dea
ibtSdan <ibtsdan@gmail.com>
2026-03-24 15:41:23 +0900

    [Silver III] Title: 바이러스, Time: 120 ms, Memory: 14604 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.java
+++ 백준/Silver/2606. 바이러스/바이러스.java
@@ -3,20 +3,17 @@ import java.util.*;
 
 public class Main {
     static BufferedWriter bw;
-    static boolean[] visited;
-    static ArrayList<Integer>[] A;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
         int n = Integer.parseInt(br.readLine());
         int m = Integer.parseInt(br.readLine());
-        A = new ArrayList[n+1];
-        visited = new boolean[n+1];
+        ArrayList<Integer>[] A = new ArrayList[n+1];
         for (int i = 1; i <= n; i++) {
             A[i] = new ArrayList<>();
         }
+        boolean[] visited = new boolean[n+1];
         for (int i = 0; i < m; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
             int u = Integer.parseInt(st.nextToken());
@@ -24,22 +21,20 @@ public class Main {
             A[u].add(v);
             A[v].add(u);
         }
-
         Deque<Integer> dq = new ArrayDeque<>();
-        dq.addLast(1);
+        dq.add(1);
         visited[1] = true;
         int cnt = 0;
         while (!dq.isEmpty()) {
-            int now = dq.pollFirst();
+            int now = dq.poll();
             for (int i : A[now]) {
                 if (!visited[i]) {
-                    dq.addLast(i);
                     visited[i] = true;
+                    dq.add(i);
                     cnt++;
                 }
             }
         }
-
         bw.write(cnt+"\n");
         bw.flush();
     }
