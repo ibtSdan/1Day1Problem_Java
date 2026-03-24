075450106118787bd88ecd8c047ef51682bec50a
ibtSdan <ibtsdan@gmail.com>
2026-03-24 15:33:34 +0900

    [Silver I] Title: 미로 탐색, Time: 144 ms, Memory: 16684 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.java
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -11,33 +11,28 @@ public class Main {
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
         int[][] A = new int[n][m];
-        boolean[][] visited = new boolean[n][m];
-        int[][] dist = new int[n][m];
         for (int i = 0; i < n; i++) {
             String line = br.readLine();
             for (int j = 0; j < m; j++) {
-                A[i][j] = line.charAt(j) - '0';
+                A[i][j] = Integer.parseInt(line.substring(j, j+1));
             }
         }
-        dist[0][0] = 1;
-        Deque<int[]> dq = new ArrayDeque<>();
-        dq.addLast(new int[] {0,0});
-        visited[0][0] = true;
         int[] dx = {1,-1,0,0};
         int[] dy = {0,0,1,-1};
+        Deque<int[]> dq = new ArrayDeque<>();
+        dq.add(new int[] {0,0});
         while (!dq.isEmpty()) {
-            int[] now = dq.pollFirst();
-            int i = now[0], j = now[1];
+            int[] now = dq.poll();
             for (int k = 0; k < 4; k++) {
-                int x = i + dx[k], y = j + dy[k];
-                if (x >= 0 && x < n && y >= 0 && y < m && A[x][y] == 1 && !visited[x][y]) {
-                    visited[x][y] = true;
-                    dq.addLast(new int[] {x,y});
-                    dist[x][y] = dist[i][j] + 1;
+                int x = now[0] + dx[k];
+                int y = now[1] + dy[k];
+                if (x >= 0 && x < n && y >= 0 && y < m && A[x][y] == 1) {
+                    A[x][y] = A[now[0]][now[1]]+1;
+                    dq.addLast(new int[] {x, y});
                 }
             }
         }
-        bw.write(dist[n-1][m-1]+"\n");
+        bw.write(A[n-1][m-1]+"\n");
         bw.flush();
     }
 }
